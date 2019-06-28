55b88f88088b211c36a350fec5eade20ca30d7b6
milan <milanps92@gmail.com>
2019-06-28 12:43:50 +0200

    Device discovery and device details

--- app/src/main/java/com/milanps/bluetoothfinder/MainActivity.java
+++ app/src/main/java/com/milanps/bluetoothfinder/MainActivity.java
@@ -1,7 +1,14 @@
 package com.milanps.bluetoothfinder;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -11,10 +18,31 @@ public class MainActivity extends AppCompatActivity {
     TextView statusTextView;
     Button searchButton;
     ListView listView;
+    BluetoothAdapter bluetoothAdapter;
+
+    private final BroadcastReceiver broadcastReciever = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.i("Action", action);
+
+            if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                statusTextView.setText("Finished");
+                searchButton.setEnabled(true);
+            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String name = device.getName();
+                String address = device.getAddress();
+                String rssi = Integer.toString(intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE));
+                Log.i("Device Found", "Name: " + name + " Address: " + address + " RSSI: " + rssi);
+            }
+        }
+    };
 
     public void searchClicked(View view) {
         statusTextView.setText("Searching...");
         searchButton.setEnabled(false);
+        bluetoothAdapter.startDiscovery();
     }
 
     @Override
@@ -25,6 +53,16 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         searchButton = findViewById(R.id.searchButton);
         statusTextView = findViewById(R.id.statusTextView);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+        registerReceiver(broadcastReciever, intentFilter);
 
     }
 }
